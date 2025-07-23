116703b2514ebf02e2fa4159f9dd518d3699a4a2
facuvar <tu-email@ejemplo.com>
2025-07-23 17:15:44 -0300

    Add error reporting for debugging

--- import_db.php
+++ import_db.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Desactivar el límite de tiempo de ejecución para scripts largos
 set_time_limit(0);
 
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /**
  * Main entry point - redirects to login page
  */
